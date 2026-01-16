277948329525f33f28317adcbd1d7a80cd5fa57e
Luis Marquez <luis.marquez@sundevs.com>
2026-01-16 15:37:33 +0100

    chore: add gunicorn executable

---
+++ backend/gunicorn_start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+NAME="taskmanager"                                  # Name of the application
+DJANGODIR=/home/luism27/projects/task-manager/backend              # Django project root directory
+SOCKFILE=/home/luism27/projects/task-manager/backend/run/gunicorn.sock  # Gunicorn socket path
+LOGDIR=logs/                # Log directory
+USER=luism27                                # The user to run as
+GROUP=webdata                                     # The group to run as
+NUM_WORKERS=3                                     # Number of worker processes (usually 2*CPU + 1)
+DJANGO_WSGI_MODULE=taskmanager.wsgi                 # WSGI module name
+
+echo "Starting $NAME with Gunicorn"
+
+# Create the run and log directories if they don't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+test -d $LOGDIR || mkdir -p $LOGDIR
+
+# Activate the virtual environment
+source venv/bin/activate
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Start your Django Unicorn
+exec ../bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=info \
+  --log-file=$LOGDIR/$NAME.log \
+  --error-logfile=$LOGDIR/$NAME-error.log
